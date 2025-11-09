681c7ea7a06a45624f456f5609636c7b303816a9
shafayatGit <shafayathossain.ielts@gmail.com>
2025-11-09 16:38:13 +0600

    push

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const course = "Next Level Web Development Course";
+console.log(course);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const course : string = "Next Level Web Development Course";
+console.log(course);
